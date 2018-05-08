646f82061a8c6d9995717c4ce648079aa6a156e1
Rusty Martin <rusty@commercialprogression.com>
2018-05-08 16:05:06 -0400

    Fixed a couple of issues and reversed the core function to default to skipping rows, if entities exist.

--- src/Plugin/migrate/process/SkipRowIfEntityExists.php
+++ src/Plugin/migrate/process/SkipRowIfEntityExists.php
@@ -8,11 +8,11 @@ use Drupal\migrate\Row;
 use Drupal\migrate\MigrateSkipRowException;
 
 /**
- * Skip processing the current row when a destination entity does not exist.
+ * Skip processing the current row when a destination entity exists (or not).
  *
- * The skip_row_if_entity_not_exist process plugin checks whether an entity with
- * given entity information exists. If the entity exists, it is returned.
- * Otherwise, a MigrateSkipRowException is thrown.
+ * The skip_row_if_entity_exists process plugin checks whether an entity with
+ * given entity information exists. If the entity exists, the source value is
+ * returned. Otherwise, a MigrateSkipRowException is thrown.
  *
  * Available configuration keys:
  * - entity_type: The destination entity to search.  Defaults to "node".
@@ -28,24 +28,25 @@ use Drupal\migrate\MigrateSkipRowException;
  *      migration: users_migration
  *      source: uuid
  *    -
- *      # Check if a node exists in the destination database.
- *      plugin: skip_row_if_entity_not_exist
+ *      # Check to make sure a user entity exists in the destination database.
+ *      plugin: skip_row_if_entity_exists
  *      entity:_type profile
  *      property_name: uid
+ *      inverse_check: FALSE
  *      message: 'User entity not found.'
  * @endcode
  *
- * This will return the uid found in the migration_lookup plugin, if one exists.
- * Otherwise, the row will be skipped and the message will be logged.
+ * This will return the source value if a user entity exists. Otherwise, the
+ * row will be skipped and the message will be logged.
  *
  * @see \Drupal\migrate\Plugin\MigrateProcessInterface
  *
  * @MigrateProcessPlugin(
- *   id = "skip_row_if_entity_not_exist",
+ *   id = "skip_row_if_entity_exists",
  *   handle_multiples = TRUE
  * )
  */
-class SkipRowIfEntityNotExist extends ProcessPluginBase {
+class SkipRowIfEntityExists extends ProcessPluginBase {
 
   /**
    * The entity type being searched.
@@ -62,17 +63,26 @@ class SkipRowIfEntityNotExist extends ProcessPluginBase {
   protected $property_name = 'nid';
 
   /**
+   * Force the entity check to be inverse.
+   */
+  protected $inverse_check = FALSE;
+
+  /**
    * {@inheritdoc}
    */
   function __construct($configuration, $plugin_id, $plugin_definition) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
 
-    if (!empty($configuration['entity'])) {
-      $this->entity = $configuration['entity'];
+    if (!empty($configuration['entity_type'])) {
+      $this->entity_type = $configuration['entity_type'];
     }
 
-    if (!empty($configuration['property'])) {
-      $this->property = $configuration['property'];
+    if (!empty($configuration['property_name'])) {
+      $this->property_name = $configuration['property_name'];
+    }
+
+    if (!empty($configuration['inverse_check'])) {
+      $this->inverse_check = $configuration['inverse_check'];
     }
   }
 
@@ -80,13 +90,15 @@ class SkipRowIfEntityNotExist extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $count = \Drupal::entityQuery($this->entity)
-      ->condition($this->property, $value)
+    $count = \Drupal::entityQuery($this->entity_type)
+      ->condition($this->property_name, $value)
       ->accessCheck(FALSE)
       ->count()
       ->execute();
 
-    if (!$count) {
+    // If there is a matching entity and inverse check is false or if there is
+    // no matching entity and inverse check is true, we want to fail this row.
+    if ($count && $this->inverse_check === FALSE || !$count && $this->inverse_check === TRUE) {
       $message = isset($this->configuration['message']) ? $this->configuration['message'] : '';
       throw new MigrateSkipRowException($message);
     }
